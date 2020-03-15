294d3134c264801fba6dfb3dc318882ee62d8c07
Nikita Kuzminov <nkuzminov220299@gmail.com>
2020-03-15 11:02:42 +0300

    new_branch

--- indexJS.js
+++ indexJS.js
@@ -1,7 +1,14 @@
+document.addEventListener('DOMContentLoaded', (event) => {
+    
+    const h1 = document.getElementById("el");
+    if (h1 === undefined) {
+        console.log('Hi');
+        h1.innerText = 'Hi';
+    } else {
+        for (let i = 0; i < 9; i++) {
+        console.log('Buy');
+        h1.innerText = h1.innerText +'Buy\n';
+        }
+    }
 
-var input = 5;
-if (input === undefined) {
-    console.log('Hi');
-} else {
-    console.log('Buy');
-}
+});
